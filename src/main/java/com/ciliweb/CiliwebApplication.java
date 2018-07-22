b06e08bce8573168f3c920c73f570e67d123c135
yfxx <1336874736@qq.com>
2018-07-22 15:30:08 +0800

    first commit

---
+++ src/main/java/com/ciliweb/CiliwebApplication.java
@@ -0,0 +1,12 @@
+package com.ciliweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiliwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiliwebApplication.class, args);
+	}
+}
